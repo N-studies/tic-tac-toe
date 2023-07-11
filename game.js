c182d3585061af0e0736f5137bfe4bd360e7be9b
N-Studies <122186098+N-studies@users.noreply.github.com>
2023-07-11 09:49:17 -0400

    add clear board feature

--- game.js
+++ game.js
@@ -9,98 +9,14 @@ const playerTwo = player("Plater Two", "O");
 /*------Board------*/
 
 const gameBoard = (() => {
-  // We will manually fill board for now
   const boardArr = [
-    [, ,],
-    [, ,],
-    [, ,],
+    ["","" ,""],
+    ["", "",""],
+    ["","" ,""],
   ];
 
   return { boardArr };
 })();
-
-/*
-function addMarksToDom(person) {
-  //add logic from array to board
-  const squares = document.querySelectorAll(".square");
-  squares.forEach((square) => {
-    if (square.textContent == "X") {
-      removeEventListener("click", () => {});
-    } else if (square.textContent == "O") {
-      removeEventListener("click", () => {});
-    } else {
-      square.addEventListener("click", (e) => {
-        e.target.textContent = person.mark;
-        square.setAttribute(
-          "style",
-          "font-size: 60px; display: flex; justify-content:center; align-items:center; font-family: sans-serif;"
-        );
-        // Add to Array
-        gameBoard.boardArr[square.dataset.position[0]][
-          square.dataset.position[1]
-        ] = person.mark;
-      });
-    }
-  });
-} */
-
-/*------Game Flow------*/
-/*const checkForWinner = () => {
-  // Check for winner
-  // can consolidate this!!!!
-  // Check rows
-  let winner = "No Winner";
-  const allX = (currentValue) => currentValue == "X";
-  const allO = (currentValue) => currentValue == "O";
-  gameBoard.boardArr.forEach((row) => {
-    if (row.every(allX)) {
-      winner = "Player One"; // replace w/ return
-    } else if (row.every(allO)) {
-      winner = "Player Two";
-    }
-  });
-
-  // Check columns
-  let columns = gameBoard.boardArr[0].map(
-    (_, colIndex) => gameBoard.boardArr.map((row) => row[colIndex]) //transpose
-  );
-  columns.forEach((column) => {
-    if (column.every(allX)) {
-      winner = "Player One"; // replace w/ return
-    } else if (column.every(allO)) {
-      winner = "Player Two";
-    }
-  });
-
-  //Check diagonals
-  diagonals = [
-    [
-      gameBoard.boardArr[0][0],
-      gameBoard.boardArr[1][1],
-      gameBoard.boardArr[2][2],
-    ],
-    [
-      gameBoard.boardArr[0][2],
-      gameBoard.boardArr[1][1],
-      gameBoard.boardArr[2][0],
-    ],
-  ];
-
-  diagonals.forEach((diagonal) => {
-    if (diagonal.every(allX)) {
-      winner = "Player One"; // replace w/ return
-    } else if (diagonal.every(allO)) {
-      winner = playerTwo;
-    }
-  });
-  // return true or false depending on the winner.
-
-  if (winner == "No Winner") {
-    return "No one won yet.";
-  } else {
-    return `Winner: ${winner}`;
-  }
-}; */
 
 /*------Gameplay------ */
 const game = () => {
@@ -108,23 +24,27 @@ const game = () => {
     //add logic from array to board
     const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
-      square.addEventListener("click", (e) => {
-        e.target.textContent = person.mark;
+      square.addEventListener(
+        "click",
+        (e) => {
+          e.target.textContent = person.mark;
+          gameBoard.boardArr[square.dataset.position[0]][
+            square.dataset.position[1]
+          ] = person.mark;
 
-        if (person == playerOne) {
-          person = playerTwo;
-        } else if ((person = playerTwo)) {
-          person = playerOne;
-        }
-        square.setAttribute(
-          "style",
-          "font-size: 60px; display: flex; justify-content:center; align-items:center; font-family: sans-serif;"
-        );
-        // Add to Array
-        gameBoard.boardArr[square.dataset.position[0]][
-          square.dataset.position[1]
-        ] = person.mark;
-      },{once:true});
+          if (person == playerOne) {
+            person = playerTwo;
+          } else if ((person = playerTwo)) {
+            person = playerOne;
+          }
+          square.setAttribute(
+            "style",
+            "font-size: 60px; display: flex; justify-content:center; align-items:center; font-family: sans-serif;"
+          );
+          // Add to Array
+        },
+        { once: true }
+      );
     });
   };
   const checkWinner = () => {
@@ -175,7 +95,6 @@ const game = () => {
         winner = playerTwo;
       }
     });
-    // return true or false depending on the winner.
 
     if (winner == "No Winner") {
       return "No one won yet";
@@ -183,12 +102,22 @@ const game = () => {
       return `Winner: ${winner}`;
     }
   };
-  
+
+  const clearBoard = () => {
+    squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.textContent = "";
+    });
+    gameBoard.boardArr.forEach((row) => {
+      row.fill("");
+    });
+  };
+
   const playerTurns = (player) => {
     addMarks(player);
-    checkWinner();
+    //checkWinner();
   };
-  return { playerTurns };
+  return { playerTurns, clearBoard };
 };
 
 const play = game();
@@ -196,6 +125,8 @@ const play = game();
 let startButton = document.querySelector("#start");
 startButton.addEventListener("click", () => {
   play.playerTurns(playerOne);
+  play.clearBoard()
+  startButton.textContent = "restart"
 });
 
 // start with x then switch to o
